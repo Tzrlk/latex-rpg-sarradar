414b5220f21883b9a671be290f281087a7d8af86
pcummuskey <pcummuskey@bravurasolutions.com>
2018-03-06 11:30:17 +1300

    Some tentative working changes to fix compile.

--- compile.kts
+++ compile.kts
@@ -15,40 +15,40 @@ import org.apache.velocity.app.Velocity
 import org.apache.velocity.Template
 import org.apache.velocity.VelocityContext
 
+val log = LoggerFactory.getLogger("compile")
+
+val csvformat = CSVFormat.RFC4180.withFirstRecordAsHeader()
+fun <T> readcsv(file: String, mapper: (CSVRecord) -> T) = FileReader(file)
+		.use { reader -> CSVParser(reader, csvformat)
+				.use { parser -> parser.map(mapper) } }
+
+log.info("Beginning spell data parsing.")
+val spells = readcsv("src/spells.csv") { record -> mapOf(
+	"name"        to record.get("Name"),
+	"type"        to record.get("Type"),
+	"action"      to record.get("Action"),
+	"range"       to record.get("Range"),
+	"components"  to record.get("Components"),
+	"duration"    to record.get("Duration"),
+	"description" to record.get("Description")
+) }
+
+log.info("Beginning discipline data parsing.")
+val disciplines = readcsv("src/disciplines.csv") { record -> mapOf(
+	"name"          to record.get("Name"),
+	"category"      to record.get("Category"),
+	"grade"         to record.get("Grade"),
+	"prerequisites" to record.get("Prerequisites"),
+	"description"   to record.get("Description")
+) }
+
 Velocity.init(Properties().apply {
 })
 
 val currentDate = SimpleDateFormat("dd/MM/yy")
-	.format(Date())
+		.format(Date())
 
-val spells = FileReader("src/spells.csv").use { reader ->
-	val format = CSVFormat.RFC4180.withFirstRecordAsHeader()
-	CSVParser(reader, format).use { parser ->
-		parser.map { record -> mapOf(
-			"name"        to record.get("Name"),
-			"type"        to record.get("Type"),
-			"action"      to record.get("Action"),
-			"range"       to record.get("Range"),
-			"components"  to record.get("Components"),
-			"duration"    to record.get("Duration"),
-			"description" to record.get("Description")
-		) }
-	}
-}
-
-val disciplines = FileReader("src/disciplines.csv").use { reader ->
-	val format = CSVFormat.RFC4180.withFirstRecordAsHeader()
-	CSVParser(reader, format).use { parser ->
-		parser.map { record -> mapOf(
-			"name"          to record.get("Name"),
-			"category"      to record.get("Category"),
-			"grade"         to record.get("Grade"),
-			"prerequisites" to record.get("Prerequisites"),
-			"description"   to record.get("Description")
-		) }
-	}
-}
-
+log.info("Beginning template compilation.")
 FileWriter("src/main.tex").use { writer ->
 	Velocity.mergeTemplate(
 			"src/main.tex.vm", "utf-8",
