594f5cc35951031e464c48d9f2b32a89fac6649f
pcummuskey <pcummuskey@bravurasolutions.com>
2018-03-05 13:25:36 +1300

    Making build script tolerant of locally installed tools.

--- build.sh
+++ build.sh
@@ -1,19 +1,40 @@
 #!/bin/sh
 
-if ! which docker 1>/dev/null 2>&1; then
-	echo >&2 "Couldn't find docker executable. You need this."
-	exit 1
-fi
+bin_docker=`which docker 2>/dev/null`
+bin_kscript=`which kscript 2>/dev/null`
+bin_pdflatex=`which pdflatex 2>/dev/null`
 
 WORK=`pwd`
 
+if [ -z "${bin_kscript}" ] || [ -z "${bin_pdflatex}" ]; then
+	
+	echo "Missing required tools. Substituting via docker."
+	if [ -z "${bin_docker}" ]; then
+		echo >&2 "Docker not available."
+		exit 1
+	fi
+
+	registry=${DOCKER_REGISTRY}
+	if [ ! -z "${registry}" ]; then
+		registry="${registry}/"
+	fi
+
+	cmd_common="docker run -it --rm -v \"${WORK}\"://work -w //work"
+
+	if [ -z "${bin_kscript}" ]; then
+		echo "Missing kscript substituted for docker command."
+		bin_kscript="${cmd_common} ${registry}tzrlk/kscript"
+	fi
+
+	if [ -z "${bin_pdflatex}" ]; then
+		echo "Missing pdflatex substituted for docker command."
+		bin_pdflatex="${cmd_common} ${registry}schickling/latex"
+	fi
+
+fi
+
 # Compile the velocity templates
-docker run -it --rm \
-	--volume  "${WORK}":/work \
-	--workdir /work \
-	--entrypoint /root/.sdkman/candidates/kscript/current/bin/kscript \
-	tzrlk/kscript \
-		compile.kts
+eval ${bin_kscript} compile.kts
 
 # Check that compilation succeeded.
 if [ ${?} ]; then
@@ -25,14 +46,11 @@ fi
 mkdir -p _build
 
 # Compile the latex to pdf
-docker run -it --rm \
-	--volume  "${WORK}":/work \
-	--workdir /work \
-	schickling/latex \
-		-halt-on-error \
-		-interaction nonstopmode \
-		-output-directory _build \
-		src/main.tex
+eval ${bin_pdflatex} \
+	-halt-on-error \
+	-interaction nonstopmode \
+	-output-directory _build \
+	src/main.tex
 
 if [ ${?} ]; then
 	echo >&2 "LaTeX compilation failed."
